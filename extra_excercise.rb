92a00d26efffc3080db92ccd58051f5608556113
Camiyah Goins <tokyonote9@gmail.com>
2020-04-22 23:04:48 +0000

    Automatically backed up by Learn

--- extra_excercise.rb
+++ extra_excercise.rb
@@ -1,8 +1,8 @@
 jewels_in_bag= 100
 
-3.times do
+10.times do
   puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 15
+  jewels_in_bag = jewels_in_bag - 10
   puts "Now there are only #{jewels_in_bag} jewels left to hide!"
 end
 
